8bc1f23dd09314affa20d309bd19186c4961b870
Benjamin Brieber <bbrieber@users.noreply.github.com>
2022-10-01 08:38:38 +0200

    Music

---
+++ project/src/Autoload/MusicManager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+onready var music_player: AudioStreamPlayer = get_node("MusicPlayer")
+export var sound_file_path : String = ""
+var music = preload("res://src/music/LD51.ogg")
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	music_player.stream = music
+	play_music() # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func play_music():
+	music_player.play()
+	
+func stop_music():
+	music_player.stop()
